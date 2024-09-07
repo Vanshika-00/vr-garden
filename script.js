b6aed48bd2fc685f9c354714b7fa60de2bf2b972
vanshika <vanshikamehra131@gmail.com>
2024-09-07 13:31:33 +0530

    colored-ver

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 const images = [
     "images/im-1.jpg",
     "images/im-2.jpg",
-    "image3.jpg",
-    "image4.jpg",
-    "image5.jpg",
-    "image6.jpg",
-    "image7.jpg",
-    "image8.jpg",
-    "image9.jpg",
-    "image10.jpg",
-    "image11.jpg",
-    "image12.jpg",
-    "image13.jpg",
-    "image14.jpg",
-    "image15.jpg",
-    "image16.jpg",
-    "image17.jpg",
-    "image18.jpg",
-    "image19.jpg",
-    "image20.jpg"
+    "images/im-3.jpg",
+    "images/im-4.jpg",
+    "images/im-5.jpg",
+    "images/im-6.jpg",
+    "images/im-7.jpg",
+    "images/im-8.jpg",
+    "images/im-9.jpg",
+    "images/im-10.jpg",
+    "images/im-11.jpg",
+    "images/im-12.jpg",
+    "images/im-13.jpg",
+    "images/im-14.jpg",
+    "images/im-15.jpg",
+    "images/im-16.jpg",
+    "images/im-17.jpg",
+    "images/im-18.jpg",
+    "images/im-19.jpg",
+    "images/im-20.jpg",
+    
+ 
 ];
 
 function generateRandomNumber() {
-    const randomNumber = Math.floor(Math.random() * 2) + 1; 
-    const image = images[randomNumber - 1];
-
-    document.getElementById('fact-image').src = image;
+    const randomNumber = Math.floor(Math.random() * 20) + 1;
+    const image = images[randomNumber - 1]; 
+    document.getElementById('fact-image').src = image; 
 }
